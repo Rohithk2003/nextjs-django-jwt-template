1ef5f65130375cd36cf21a2d3294a1915f067b1d
Rohith Krishnan <rohithk652@gmail.com>
2024-08-13 18:11:22 +0530

    add faq

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,10 +1,12 @@
 import AboutUs from "@/components/aboutus";
 import Contact from "@/components/contact";
+import Faq from "@/components/faq";
 
 export default function Home() {
 	return (
 		<main>
 			<AboutUs />
+			<Faq />
 			<Contact />
 		</main>
 	);
