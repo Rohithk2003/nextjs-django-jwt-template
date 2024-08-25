ec0ad76a1626759c275999779f617ae1135a2b44
Rohithk <67067796+Rohithk2003@users.noreply.github.com>
2024-08-25 18:38:43 +0530

    add .env

--- frontend/app/dashboard/page.tsx
+++
@@ -1,9 +0,0 @@
-import { getServerSession } from "next-auth";
-import React from "react";
-import { authOptions } from "../api/auth/[...nextauth]/auth";
-
-export default async function Dashboard() {
-	const session = await getServerSession(authOptions);
-	console.log(session);
-	return <div>Dashboard</div>;
-}
